73ad60411009c847e938ec5bb57f0730084b3150
Rodolfo Marques Ferreira de Abreu <rodolfo.abreu@heat.local>
2019-09-09 16:04:45 -0300

    remocao de abas

--- navigation/MainTabNavigator.js
+++ navigation/MainTabNavigator.js
@@ -86,8 +86,8 @@ SettingsStack.path = '';
 
 const tabNavigator = createBottomTabNavigator({
   HomeStack,
-  LinksStack,
-  SettingsStack,
+  //LinksStack,
+  //SettingsStack,
 });
 
 tabNavigator.path = '';
